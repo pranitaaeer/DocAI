89f998ed8e5bdf70162067d12276ec2b2de66ce3
pranitaaeer <aeerpranita@gmail.com>
2026-08-19 17:46:05 +0530

    add multer middleware

---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10 MB
+  },
+  fileFilter: (_req, file, cb) => {
+    if (file.mimetype === "application/pdf") {
+      cb(null, true);
+    } else {
+      cb(new Error("Only PDF files are allowed"));
+    }
+  },
+});
+
+export default upload;
