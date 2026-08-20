aac5ff2664df46546d0eedc53bc526c8bdc428f6
pranitaaeer <aeerpranita@gmail.com>
2026-08-20 14:07:17 +0530

    write function to connect with chroma db

---
+++ backend/src/config/chroma.ts
@@ -0,0 +1,10 @@
+import { CloudClient } from "chromadb";
+
+
+const chromaClient = new CloudClient({
+  apiKey: process.env.CHROMA_API_KEY,
+  tenant: process.env.CHROMA_TENANT,
+  database: process.env.CHROMA_DATABASE,
+});
+
+export default chromaClient;
