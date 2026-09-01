278e042052414d137d4051084ba1002e798b349e
Pranita Aeer <aeerpranita@gmail.com>
2026-09-01 16:14:16 +0530

    Update CORS origin for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieparser())
 app.use(cors({
-    origin:"https://docai-t7rl.onrender.com",
+    origin:"https://doc-ai-wheat.vercel.app",
     credentials:true
 }))
 
